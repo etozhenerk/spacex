db29baaea0c96a778942b3432c5733e6eac398cb
Mikhail <etozhenerk@yandex.ru>
2021-09-26 21:17:50 +0300

    add updateRocket to app.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import { Features } from "./components/Features/Features";
 import "./style.css";
 
 class App extends React.Component {
-
   fetchData = new FetchData();
 
   state = {
     rocket: "Falcon 1",
+    rocketFeatures: null,
   };
 
+  componentDidMount() {}
 
+  updateRocket() {
+    this.fetchData
+      .getRocket()
+      .then((data) => data.find((item) => item.name === this.state.rocket))
+      .then((rocketFeatures) => this.setState({ rocketFeatures }));
+  }
 
   render() {
     return (
