3214faf0b24ce7a7422de7467f1e84729d9aecef
Mikhail <etozhenerk@yandex.ru>
2021-09-26 21:57:39 +0300

    add props to header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,21 @@ import React from "react";
 import logo from "../../logo.svg";
 import "./header.css";
 
-export const Header = ({ rockets }) => (
+export const Header = ({ rockets, changeRocket }) => (
   <header className="header">
     <img src={logo} alt="Logo Space X" className="logo" />
     <nav className="main-nav nav">
       <ul className="list">
         {rockets.map((rocket, i) => (
           <li key={i} className="item">
-            <a href="/" className="item-link">
+            <a
+              href="/"
+              className="item-link"
+              onClick={(e) => {
+                e.preventDefault();
+                changeRocket(rocket);
+              }}
+            >
               {rocket}
             </a>
           </li>
